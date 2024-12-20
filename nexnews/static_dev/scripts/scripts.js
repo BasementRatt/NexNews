0f290475c91918b0645ad95f10531330590f9c9d
Денис Баранов <finedend@gmail.com>
2024-12-20 17:37:41 +0300

    Добавление sidebar и изменение footer при помощи css

--- nexnews/static_dev/scripts/scripts.js
+++ nexnews/static_dev/scripts/scripts.js
@@ -1,9 +1,10 @@
-// При нажатии на кнопку "НАЧАТЬ"
-document.getElementById('start-btn').addEventListener('click', function() {
-    // Анимация для скрытия заголовка и кнопки
-    document.getElementById('main-header').style.transform = 'translate(-50%, -200%)'; // Поднимаем вверх
-    document.getElementById('main-header').style.opacity = '0';
-    document.getElementById('start-btn').style.opacity = '0';
+document.addEventListener('DOMContentLoaded', function() {
+    // При нажатии на кнопку "НАЧАТЬ"
+    document.getElementById('start-btn').addEventListener('click', function() {
+        // Анимация для скрытия заголовка и кнопки
+        document.getElementById('main-header').style.transform = 'translate(-50%, -200%)'; // Поднимаем вверх
+        document.getElementById('main-header').style.opacity = '0';
+        document.getElementById('start-btn').style.opacity = '0';
 
     // Плавный переход на статьи
     setTimeout(function() {
@@ -13,16 +14,29 @@ document.getElementById('start-btn').addEventListener('click', function() {
     }, 1000); // Задержка, чтобы анимация была плавной
 });
 
-// Показать модальное окно
-const helpBtn = document.getElementById('help-btn');
-const modal = document.getElementById('modal');
-const closeModal = document.getElementById('close-modal');
 
-helpBtn.addEventListener('click', () => {
+    //sidebar
+    document.getElementById('toggleBtn').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.toggle('open');
+        document.querySelector('.content').classList.toggle('shift');
+    });
+
+    document.getElementById('closeBtn').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.remove('open');
+        document.querySelector('.content').classList.remove('shift');
+    });
+
+    // Показать модальное окно
+    const helpBtn = document.getElementById('help-btn');
+    const modal = document.getElementById('modal');
+    const closeModal = document.getElementById('close-modal');
+
+    helpBtn.addEventListener('click', () => {
     modal.style.display = 'flex'; // Показать модальное окно
-});
+    });
 
-// Закрыть модальное окно
-closeModal.addEventListener('click', () => {
+    // Закрыть модальное окно
+    closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
+    });
 });
