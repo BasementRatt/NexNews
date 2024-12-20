8494ab45ff36627035d4d40a839e519a0ed97cfd
Денис Баранов <finedend@gmail.com>
2024-12-20 18:21:49 +0300

    Появление sidebar после нажатия на кнопку НАЧАТЬ

--- nexnews/static_dev/scripts/scripts.js
+++ nexnews/static_dev/scripts/scripts.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const helpBtn = document.getElementById('help-btn');
+    const modal = document.getElementById('modal');
+    const closeModal = document.getElementById('close-modal');
+    const startBtn = document.getElementById('start-btn');
+    const main_header = document.getElementById('main-header');
+    const footer = document.getElementById('site-footer');
+    const toggleBtn = document.getElementById('toggleBtn')
     // При нажатии на кнопку "НАЧАТЬ"
-    document.getElementById('start-btn').addEventListener('click', function() {
+    startBtn.addEventListener('click', function() {
         // Анимация для скрытия заголовка и кнопки
-        document.getElementById('main-header').style.transform = 'translate(-50%, -200%)'; // Поднимаем вверх
-        document.getElementById('main-header').style.opacity = '0';
-        document.getElementById('start-btn').style.opacity = '0';
-
-    // Плавный переход на статьи
-    setTimeout(function() {
-        document.querySelector('.main-content').style.display = 'block'; // Показываем контент
-        document.querySelector('.main-content').style.opacity = '1'; // Плавно показываем контент
-        document.body.style.background = '#fff'; // Меняем фон на белый
-    }, 1000); // Задержка, чтобы анимация была плавной
+        main_header.style.transform = 'translate(-50%, -200%)'; // Поднимаем вверх
+        main_header.style.opacity = '0';
+        startBtn.style.opacity = '0';
+        
+        // Плавный переход на статьи
+        setTimeout(function() {
+            toggleBtn.style.display = 'block';
+            footer.style.display = 'block';
+            document.querySelector('.main-content').style.display = 'block'; // Показываем контент
+            document.querySelector('.main-content').style.opacity = '1'; // Плавно показываем контент
+            document.body.style.background = '#fff'; // Меняем фон на белый
+        }, 500); // Задержка, чтобы анимация была плавной
 });
-
-
     //sidebar
     document.getElementById('toggleBtn').addEventListener('click', function() {
         document.getElementById('sidebar').classList.toggle('open');
@@ -27,10 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Показать модальное окно
-    const helpBtn = document.getElementById('help-btn');
-    const modal = document.getElementById('modal');
-    const closeModal = document.getElementById('close-modal');
-
     helpBtn.addEventListener('click', () => {
     modal.style.display = 'flex'; // Показать модальное окно
     });
