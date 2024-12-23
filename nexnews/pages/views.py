c8f2900a7534aa5507f4f2d5e23208493eec5a57
BasementRat <xolegnov2004@gmail.com>
2024-12-23 10:33:27 +0300

    Сделал контакты рабочими и снёс вкладку новости

--- nexnews/pages/views.py
+++ nexnews/pages/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def about(request):
     template = 'pages/about.html'
     return render(request, template)
+
+def contact(request):
+    template = 'pages/contact.html'
+    return render(request, template)
